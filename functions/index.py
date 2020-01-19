52d4160d27fa4f901ac26a95385a34c16c99176a
Bishoy Romany <7bishoyromany7@gmail.com>
2020-01-19 23:05:47 +0200

    lessons from 36 to 40

--- functions/index.py
+++ functions/index.py
@@ -35,9 +35,42 @@ def fullPerson(name , **data):
     forData = {'name' : name}
     for v,k in data.items():
         forData[k] = v
-
     return forData
+
+# global vars
+b = 10
+def bIsGloblaHere():
+    # global b
+    # or
+    b = 10
+    x = globals()['b']
+    x = 20
+    return [b , x]
+
+# list
+lst = [5,10,30,40,100,52,60 , 75]
+def oddEven(lis):
+    odd = 0
+    even = 0
+    for i in lis:
+        if i%2 == 0:
+            even += 1
+        else:
+            odd += 1
+    return {'odd' : odd , 'even' : even}
+
+
+def fib(n):
+    nums = []
+    for i in range(n):
+        nums.append(i)
+        if i < 2:
+            nums[i] = i
+        else:
+            nums[i] = nums[i - 1] + nums[i - 2]
+    return nums
 
 r1,r2 = add_sub(10,20)
 print(greet() , returnThis('wroking') , multipleThis(10) , selfParams(10) , r1 , r2 ,person(name="name" , age="age") ,
-      sumIt(10 , 20 , 20 , 40 , 50 , 60 , 100) , fullPerson('Bishoy' , city="Cairo" , phone="1234" , country="egy" , age=18))
+      sumIt(10 , 20 , 20 , 40 , 50 , 60 , 100) , fullPerson('Bishoy' , city="Cairo" , phone="1234" , country="egy" , age=18) ,
+      bIsGloblaHere() , b , oddEven(lst) , fib(20))
